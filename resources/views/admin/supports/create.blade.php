7b9e5cf94942f3807659eb5cf0239070fa88c401
jo0u <joualexandre120@hotmail.com>
2025-07-06 20:32:52 -0300

    Criação de Request, para validação do Support , criamos uma classe para que toda validação seja passada por ela na função supports

--- resources/views/admin/supports/create.blade.php
+++ resources/views/admin/supports/create.blade.php
@@ -1,8 +1,13 @@
 <h1>Nova Dúvida</h1>
 
+@if($errors->any())
+    @foreach($errors->all() as $error)
+        {{$error}}
+    @endforeach
+@endif
 <form action="{{route('supports.store')}}" method="POST">
     @csrf
-    <input type="text" name="subject" id="" placeholder="Assunto">
-    <textarea name="body" id="" cols="30" rows="10" placeholder="Descrição"></textarea>
+    <input type="text" name="subject" id="" placeholder="Assunto" value="{{old('subject')}}">
+    <textarea name="body" id="" cols="30" rows="10" placeholder="Descrição">{{old('body')}}</textarea>
     <button type="submit">Enviar</button>
 </form>
