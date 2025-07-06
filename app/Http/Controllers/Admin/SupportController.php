ea79e5c9195ea4e32b266ff74d0c43c45e4e8f9a
jo0u <joualexandre120@hotmail.com>
2025-07-06 20:08:06 -0300

    Criação da função update do supports

--- app/Http/Controllers/Admin/SupportController.php
+++ app/Http/Controllers/Admin/SupportController.php
@@ -51,4 +51,32 @@ class SupportController extends Controller
     return redirect()->route('supports.index');
      
     }
+
+    public function edit(Support $support ,string|int $id)
+    {
+        if(! $support = $support->where('id',$id)->first()){
+            return back();
+        }
+
+        return view('admin.supports.edit', compact('support'));
+    }
+
+    public function update(Request $request,Support $support,string|int $id)
+    {
+          if(! $support = $support->find($id)){
+        return back();
+       }
+
+
+       $support->update($request->only([
+        'subject',
+        'body'
+       ]));
+
+
+       return redirect()->route('supports.index');
+
+    }
+
+
 }
