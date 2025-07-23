18f5eedb4efa05ff471621042ec56386effcc68c
jo0u <joualexandre120@hotmail.com>
2025-07-23 18:49:10 -0300

    criação de services para melhor divisão de funções no supports

--- app/Http/Controllers/Admin/SupportController.php
+++ app/Http/Controllers/Admin/SupportController.php
@@ -5,29 +5,38 @@ namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\StoreUpdateSupport;
 use App\Models\Support;
+use App\Services\SupportService;
 use Illuminate\Http\Request;
 
 class SupportController extends Controller
 {
-    public function index(Support $support)
+
+
+
+    public function __construct(
+        protected SupportService $service
+    ){}
+
+
+    public function index(Request $request)
     {
 
         
-        $supports = $support->all();
+        $supports = $this->service->getAll($request->filter);
 
        
-        return view('admin.supports.index', compact('supports'));
+        return view("admin.supports.index", compact('supports'));
     }
 
 
 
-    public function show(string|int $id, Support $support)
+    public function show(string $id)
     {
        //$support = Support::find($id);
        //$support = Support::findOrFail($id);
        //$support = Support::where('id', '=' ,$id)->first();
        
-       if(! $support = Support::find($id)){
+       if(! $support = $this->service->findOne($id)){
         return back();
        }
 
@@ -55,9 +64,10 @@ class SupportController extends Controller
      
     }
 
-    public function edit(Support $support ,string|int $id)
+    public function edit(string $id)
     {
-        if(! $support = $support->where('id',$id)->first()){
+       // if(! $support = $support->where('id',$id)->first()){
+        if(! $support = $this->service->findOne($id)){
             return back();
         }
 
@@ -78,13 +88,10 @@ class SupportController extends Controller
 
     }
 
-    public function destroy(string|int $id, Support $support)
+    public function destroy(string $id)
     {
-            if(! $support = $support->find($id)){
-        return back();
-       }
-
-       $support->delete();
+      
+        $this->service->delete($id);
 
        return redirect()->route('supports.index');
     }
