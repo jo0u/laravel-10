8bf60d2d6f545b77889bed3acf7de6188f9d4aa4
jo0u <joualexandre120@hotmail.com>
2025-07-23 19:06:27 -0300

    Trabalhando com padrão DTO, fazendo modificações fazendo que o proprio DTO crie uma classe e faça que os dados seja preenchidos com mais segurança

--- app/Http/Controllers/Admin/SupportController.php
+++ app/Http/Controllers/Admin/SupportController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers\Admin;
 
+use App\DTO\CreateSupportDTO;
+use App\DTO\UpdateSupportDTO;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\StoreUpdateSupport;
 use App\Models\Support;
@@ -55,11 +57,9 @@ class SupportController extends Controller
 
     public function store(StoreUpdateSupport $request, Support $support)
     {
-     $data = $request->validated();
-     $data['status'] = 'a';
-
-    $support->create($data);
-    
+        $this->service->new( 
+            CreateSupportDTO::makeFromRequest($request)
+        );
     return redirect()->route('supports.index');
      
     }
@@ -76,12 +76,12 @@ class SupportController extends Controller
 
     public function update(StoreUpdateSupport $request,Support $support,string|int $id)
     {
-          if(! $support = $support->find($id)){
+         $support = $this->service->update(
+            UpdateSupportDTO::makeFromRequest($request)
+          );
+          if(! $support){
         return back();
        }
-
-
-       $support->update($request->validated());
 
 
        return redirect()->route('supports.index');
